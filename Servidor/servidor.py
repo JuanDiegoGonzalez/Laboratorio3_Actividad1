1e857cd4e748451770f0f9fa6d4d3b6f6b555b2b
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-10-04 12:58:08 -0500

    Version final

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,4 +1,4 @@
-import socket, threading, hashlib, time, os
+import socket, threading, hashlib, time, os, platform, subprocess, ipaddress
 from datetime import datetime
 
 # Declaracion de atributos
@@ -97,16 +97,28 @@ if __name__ == "__main__":
         if cantConexiones < 1:
             raise ValueError("[Error] El numero debe ser mayor a 0")
 
-        print("\nServidor listo para atender clientes")
-
         # Se crea el socket del servidor (donde recibe a los clientes)
         s = socket.socket()
-        host = socket.gethostname()
+        host = None
+
+        try:  # Identificar la ip automaticamente
+            if platform.system() == 'Windows':
+                p = subprocess.Popen('ipconfig', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                host = str(p.stdout.readlines()[7]).split(" ")[-1].split("\\")[0]  # host = 192.168.0.2 (o similar)
+            elif platform.system() == 'Linux':
+                p = subprocess.Popen('ifconfig', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print(str(p.stdout.readlines()[1]).split("inet")[1].split(" ")[1])  # host = 192.168.0.16 (o similar)
+
+            ipaddress.IPv4Network(host)
+
+        except:  # Si no se puede (es decir, si hay error), se le pregunta al usuario
+            host = input("Ingrese la direccion IP de esta maquina (ejecutar el comando ipconfig o ifconfig en una terminal): ")
+
         port = 1234
         s.bind((host, port))
         s.listen(25)
-        print("Direccion IP del servidor:", socket.gethostbyname(socket.getfqdn()))
-        
+        print("\nDireccion IP del servidor:", host)
+
         # Se crea la carpeta para guardar el log (si no existe)
         if not os.path.isdir('Logs'):
             os.mkdir(os.path.join(os.getcwd(), "Logs"))
@@ -114,6 +126,8 @@ if __name__ == "__main__":
         # Se inicializan las listas de clientes
         resultComprobacionHash = [None for i in range(cantConexiones)]
         tiemposDeTransmision = [None for i in range(cantConexiones)]
+
+        print("\nServidor listo para atender clientes")
 
         # Se reciben y se atienden a los clientes
         while True:
